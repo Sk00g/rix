ccc421b4c885e4eb6a0fe2ff469cfde693b613b3
Scott Kemperman <skemperman@ascendentgroup.com>
2020-10-09 23:41:06 -0600

    stuff got done

--- public/src/index.js
+++ public/src/index.js
@@ -1,13 +1,11 @@
 import * as PIXI from "pixi.js";
 import SUIE from "./sengine/suie/suie.js";
 import utils from "./sengine/utils.js";
-import UnitAvatar from "./sengine/unitAvatar.js";
 import Keyboard from "pixi.js-keyboard";
 import Mouse from "pixi.js-mouse";
+import TileMap from "./tilemap.js";
 
 const loader = PIXI.Loader.shared;
-
-let unit, enemy, panel;
 
 PIXI.utils.sayHello("WebGL");
 PIXI.settings.RESOLUTION = 1.5;
@@ -17,81 +15,30 @@ let app = new PIXI.Application({ width: 800, height: 600, backgroundColor: 0xa0a
 document.body.appendChild(app.view);
 
 // Load images into WebGL compatible format
-let imagePaths = ["bardo_1.png", "executioner_1.png", "knights_1x.png"].map(
-    (path) => `graphics/characters/${path}`
-);
-imagePaths.push("graphics/ui/source/source.png");
+let imagePaths = ["graphics/tilesets/grassBiome/overworld_tileset_grass.png"];
+
 loader.add(imagePaths).load(() => {
     console.log("finished loading graphics");
 
-    unit = new UnitAvatar("graphics/characters/knights_1x.png");
-    unit.sprite.position.set(200, 200);
-
-    enemy = new UnitAvatar(
-        "graphics/characters/knights_1x.png",
-        new PIXI.Rectangle(156, 2, 26, 36)
+    let testMap = new TileMap(
+        app.stage,
+        "graphics/tilesets/grassBiome/overworld_tileset_grass.png",
+        [10, 10],
+        [16, 16]
     );
-    enemy.sprite.position.set(100, 200);
-
-    panel = new SUIE.Panel(
-        new PIXI.Rectangle(500, 50, 100, 100),
-        SUIE.PanelSize.LARGE,
-        SUIE.PanelColor.ORANGE
-    );
-    panel.addMember(new SUIE.Label("Hello scott", [15, 10]));
-    panel.addMember(new SUIE.TextButton("log", [20, 20], () => console.warn("booyah!")));
-    panel.addMember(new SUIE.TextButton("logi", [20, 40]));
-    panel.addMember(new SUIE.TextButton("login", [20, 60]));
-    panel.addMember(new SUIE.TextButton("logine", [20, 80]));
-    panel.addMember(new SUIE.TextButton("loginr", [20, 100]));
-    panel.addMember(new SUIE.TextButton("loginrest", [20, 120]));
-
-    /* 
-    app.stage.addChild(new SUIE.Panel(
-        [app.view.width / 2 - 100, app.view.height / 2 - 200, 200, 400],
-        [
-            new SUIE.Label([50, 50], 12, "#eeeeee", "Username")
-        ]
-    )) 
-    */
 
     Keyboard.events.on("released", (keyCode, event) => {
         switch (keyCode) {
             case "KeyA":
-                unit.playWalkAnimation();
-                enemy.playWalkAnimation();
-                break;
-            case "KeyB":
-                unit.stopAnimation();
-                enemy.stopAnimation();
-                break;
-            case "ArrowRight":
-                unit.setDirection("right");
-                break;
-            case "ArrowLeft":
-                unit.setDirection("left");
-                break;
-            case "ArrowDown":
-                unit.setDirection("down");
-                break;
-            case "ArrowUp":
-                unit.setDirection("up");
+                testMap.updateTileIndex(4, 4, 3, 3);
                 break;
         }
     });
-
-    app.stage.addChild(unit.sprite);
-    app.stage.addChild(enemy.sprite);
-    app.stage.addChild(panel);
 
     app.ticker.add(gameLoop);
 });
 
 function gameLoop(delta) {
-    unit.update(delta);
-    enemy.update(delta);
-    // panel.update(delta);
-
     Keyboard.update();
     Mouse.update();
 }
---
+++ public/src/tilemap.js
@@ -0,0 +1,45 @@
+import * as PIXI from "pixi.js";
+
+// Class used for displaying tilemap
+class TileMap {
+    constructor(stage, path, size, tileSize) {
+        this._tileSize = tileSize;
+        this._tileIndices = [];
+        this._tileSprites = [];
+
+        for (let x = 0; x < size[0]; x++) {
+            this._tileIndices[x] = [];
+            this._tileSprites[x] = [];
+            for (let y = 0; y < size[1]; y++) {
+                let texture = new PIXI.Texture(PIXI.BaseTexture.from(path));
+                let sprite = new PIXI.Sprite(texture);
+
+                sprite.position.set(x * tileSize[0], y * tileSize[1]);
+
+                this._tileSprites[x][y] = sprite;
+
+                this.updateTileIndex(x, y, 0, 0);
+
+                stage.addChild(sprite);
+            }
+        }
+    }
+
+    updateTileIndex(tileX, tileY, indexX, indexY) {
+        this._tileIndices[tileX][tileY] = [indexX, indexY];
+        this._tileSprites[tileX][tileY].texture.frame = new PIXI.Rectangle(
+            indexX * this._tileSize[0],
+            indexX * this._tileSize[1],
+            this._tileSize[0],
+            this._tileSize[1]
+        );
+    }
+
+    exportToDataFile(fileName) {
+        // collect all internal variables into a single data object
+        // convert said object into text (let text = JSON.stringify(data))
+        // download said text to the browser-user's computer
+    }
+}
+
+export default TileMap;
